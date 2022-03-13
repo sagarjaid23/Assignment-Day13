1773b5d392bb8d7241fef392dc99187ee8df2c08
sagarjaid23 <99254738+sagarjaid23@users.noreply.github.com>
2022-03-13 22:49:40 +0530

    Assignment Day13

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Find_Max_Problem_using_Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Max Number");
+            int output = MaxInt.MaxIntNumber(20, 25, 30);
+            Console.WriteLine(output);
+
+            Console.WriteLine("Max Number");
+            double Voutput = MaxFloat.MaxFloatNum("20.2, 25.5, 30.56");
+            Console.WriteLine(Voutput);
+
+            Console.WriteLine("Max Number");
+            string Vaoutput = MaxString.MaxstringNumber("111.3", "120.5", "125.3");
+            Console.WriteLine(Vaoutput);
+        }
+    }
+}
+
+
+        
---
+++ Utility.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Find_Max_Problem_using_Generics
+{
+    internal class Utility
+    {
+        public int ReadInt()
+        {
+            return int.Parse(Console.ReadLine());
+        }
+        public float ReadFloat()
+        {
+            return float.Parse(Console.ReadLine());
+        }
+        public String ReadString()
+        {
+            return (Console.ReadLine());
+        }
+       
+    }
+}
+    
+
